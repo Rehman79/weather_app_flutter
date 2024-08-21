b9c9207ee0a21eda7c6f47b57579be8f34eada51
Rehman Rana <115769841+Rehman79@users.noreply.github.com>
2024-08-21 18:50:22 +0500

    Building homepage UI

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:weather_app_flutter/utils/bg_cont.dart';
+import 'package:weather_app_flutter/utils/custom_text.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,14 +10,42 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: BgCont(
-            child: Row(
-              children: [
-                Column(
-                  children: [
-
-                  ],
-                )
-              ],
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 40),
+              child: Row(
+                children: [
+                  Column(
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          CustomText(content: '25',fontWeight: FontWeight.w700,fontSize: 70,),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 30.0),
+                            child: Row(
+                              children: [
+                                CustomText(content: '°',fontSize: 30,),
+                                CustomText(content: 'C',fontSize: 30,),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.location_pin,color: Colors.blue,),
+                          CustomText(content: 'Location',fontWeight: FontWeight.w700,fontSize: 20,),
+                        ],
+                      ),
+                      CustomText(content: 'Temperature Feels like something',fontSize: 16,),
+                      CustomText(content: 'Day,Time',fontSize: 16,),
+                      //shape
+                      //graph
+                      //list of shape
+                    ],
+                  )
+                ],
+              ),
             )
         )
     );
